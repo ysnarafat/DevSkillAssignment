9c9db1b41a2cf750298815e3967e93ebf702d663
Yeasin <yeasinarafat10@gmail.com>
2019-06-30 10:47:28 +0600

    Added Last Classes

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "XYZ";
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Please Enter A Valid Name");
+            }
+            else
+            {
+                string msg = "Welcome to Professional Programming with C#";
+                Console.WriteLine("Please Enter A Valid Name");
+                ///Console.WriteLine(msg.Insert(0, "Hei," + name));
+            }
+
+            int a=10,b=20,s = 10;
+            switch (s)
+            {
+                case 1:
+                    Console.WriteLine("This is one");
+                    break;
+                default:
+                    Console.WriteLine("This is 10");break;
+            }
+
+            int max = a > b ? a : b;
+            int? age = null;
+            int newAge = age ?? 20;
+            string name = "OKA";
+            Console.WriteLine(name.GetHashCode());
+
+        }
+    }
+}
