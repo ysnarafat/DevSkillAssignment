55c039278b1862bc9f708cbdb1accd22ee19719b
Yeasin <yeasinarafat10@gmail.com>
2019-07-01 22:24:31 +0600

    Problem 3 of Class 04

---
+++ Problem3/Problem3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] ara = new int[100,100];
+            int N,sum1=0,sum2=0;
+            N = Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<N;i++)
+                for (int j = 0; j < N; j++)
+                {
+                    ara[i,j]= Convert.ToInt32(Console.ReadLine());
+                }
+            for(int i = 0; i < N; i++)
+            {
+                sum1 += ara[i,i];
+            }
+            int k = N - 1;
+            for (int i = 0; i<N ; i++)
+            {
+                sum2 += ara[i,k];
+                k--;
+            }
+            Console.WriteLine("Left Diagonal Sum is: " + sum1 + " Right Diagonal Sum is: " + sum2);
+        }
+    }
+}
