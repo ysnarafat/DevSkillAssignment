c5114a2a8652fdb9ea214df6afb541bcc09da9aa
Yeasin <yeasinarafat10@gmail.com>
2019-07-01 21:30:41 +0600

    Solved Problem 2

---
+++ Problem2/Problem2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,rem,reverse=0;
+            Console.Write("Please give a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num!= 0)
+            {
+                rem = num% 10;
+                reverse = (reverse* 10) + rem;
+                num/= 10;
+            }
+            Console.Write("Result is: " + reverse);
+            num = reverse;
+            int flag = 0;
+            int  m = Convert.ToInt32(Math.Sqrt(num));
+            for (int i = 2; i <= m; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(" (Not Prime)");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                Console.WriteLine(" (Prime)");
+        }
+    }
+}
